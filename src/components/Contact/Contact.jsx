7990c2615c2fc6a32e016f01654a8b7ce9a8a735
MrAbhi698 <mrabhi670@gmail.com>
2025-07-29 02:04:25 +0530

    Done !!

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import './Contact.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import mail_icon from '../../assets/mail_icon.svg'
+import location_icon from '../../assets/location_icon.svg'
+import call_icon from '../../assets/call_icon.svg'
+
+const Contact = () => {
+
+    const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "a2885008-b16e-4332-965c-44f26e015d3e");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      alert(res.message);
+    }
+  };
+
+
+
+  return (
+    <div id='contact' className='contact'>
+        <div className="contact-title">
+            <h1>Get in touch</h1>
+            <img src={theme_pattern} alt=""/>
+        </div>
+        <div className="contact-section">
+            <div className="contact-left">
+                <h1>Let's talk </h1>
+                <p>I'm currently available to take on exciting new projects, so if you have an idea in mind or need someone to bring your vision to life, feel free to reach out at any time—I'd love to hear from you!</p>
+                <div className="contact-details">
+                    <div className="contact-detail">
+                        <img src={mail_icon} alt="" /><p>mrabhi670@gmail.com</p>
+                    </div>
+                    <div className="contact-detail">
+                        <img src={call_icon} alt="" /><p>+91 9148314969</p>
+                    </div>
+                    <div className="contact-detail">
+                        <img src={location_icon} alt="" /><p>India, Karnataka</p>
+                    </div>
+                </div>
+            </div>
+            <form onSubmit={onSubmit} className="contact-right">
+                <label htmlFor="">Your Name</label>
+                <input type="text" placeholder='Enter your name' name='name' />
+                <label htmlFor="">Your Email</label>
+                <input type="email" placeholder='Enter your email' name='email'/>
+                <label htmlFor="">Write your message here</label>
+                <textarea name="message" rows="8" placeholder='Enter your message'></textarea>
+                <button type='submit' className="contact-submit">Submit now</button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
