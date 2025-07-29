4c5945185f50b87a98e3890c799e78655babc577
MrAbhi698 <mrabhi670@gmail.com>
2025-07-29 15:47:47 +0530

    paragraph change

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,8 +7,8 @@ const Hero = () => {
   return (
     <div id='home' className='hero'>
         <img src={profile_img1} alt="" height={320} width={320}/>
-        <h1><span>I,m Mr Abhi,</span> fronted developer based in India.</h1>
-        <p>I am a fronted developer from India, Karnataka </p>
+        <h1><span>I,m Mr Abhi,</span> frontend developer based in India.</h1>
+        <p>I am a frontend developer from India, Karnataka </p>
         <div className="hero-action">
             <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
             <div className="hero-resume">My Resume</div>
